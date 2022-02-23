763ccc0f73ef536ace9ae39cf5010533c18ee21e
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2022-02-23 13:46:07 +0100

    fix css

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
+
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
-    require('postcss-import'),
-    require('autoprefixer'),
-    ...(process.env.JEKYLL_ENV == "production" ? [require('cssnano')({})] : [])
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.JEKYLL_ENV == "production" ? [require("cssnano")({ preset: "default" })] : [])
   ]
-}
+};
